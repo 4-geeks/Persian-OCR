f06a8c1c180921abac339227390a92421903b4c6
amirhosseinh77 <amirhossein261077@live.com>
2021-03-22 15:51:56 +0430

    added argparser to text_cropper.py

--- DataTools/Data_Labeling/craft_text_cropper/text_cropper.py
+++ DataTools/Data_Labeling/craft_text_cropper/text_cropper.py
@@ -2,6 +2,7 @@ import sys
 import os
 import time
 import argparse
+from tqdm import tqdm
 
 import torch
 import torch.nn as nn
@@ -69,22 +70,40 @@ def test_net(net, image, text_threshold, link_threshold, low_text, cuda, poly):
 
     return boxes, polys, ret_score_text
 
-net = CRAFT()
-net.load_state_dict(copyStateDict(torch.load('/content/drive/MyDrive/craft_mlt_25k.pth')))
-if True:
-    net = net.cuda()
-    net = torch.nn.DataParallel(net)
-    cudnn.benchmark = False
 
-net.eval()
-image = imgproc.loadImage('/content/CRAFT-pytorch/333.jpg')
-bboxes, polys, score_text = test_net(net, image, text_threshold=0.7, link_threshold=0.4, low_text=0.4, cuda=True, poly=False)
-for bbox in bboxes:
-  bbox = bbox.reshape((-1, 1, 2)).astype(int)
-  isClosed = True
-  color = (255, 0, 0) 
-  thickness = 2
-  image = cv2.polylines(image, [bbox],isClosed, color, thickness) 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, default='craft_mlt_25k.pth', help='model path')
+    parser.add_argument('--folder', type=str, default='data', help='dataset path')
+    args = parser.parse_args()
 
-plt.imshow(image)
-plt.show()
+    net = CRAFT()
+    net.load_state_dict(copyStateDict(torch.load(args.model)))
+    if True:
+        net = net.cuda()
+        net = torch.nn.DataParallel(net)
+        cudnn.benchmark = False
+    net.eval()
+
+    os.makedirs('res', exist_ok=True)
+    for item in tqdm(os.listdir(args.folder)):
+        item_name = ''.join(item.split('.')[:-1])
+        os.makedirs(f'res/{item_name}', exist_ok=True)
+        counter = 0
+        # craft
+        image = imgproc.loadImage(args.folder + '/' + item)
+        bboxes, polys, score_text = test_net(net, image, text_threshold=0.7, link_threshold=0.4, low_text=0.4, cuda=True, poly=False)
+        for bbox in bboxes:
+            xmin,ymin = bbox.min(0).astype(int)
+            xmax,ymax = bbox.max(0).astype(int) 
+            cv2.imwrite(f'res/{item_name}/{item_name}_{counter}.png', image[ymin:ymax, xmin:xmax][:,:,::-1])
+            counter += 1
+            
+            # using polygon
+            # bbox = bbox.reshape((-1, 1, 2)).astype(int)
+            # isClosed = True
+            # color = (255, 0, 0) 
+            # thickness = 2
+            # image = cv2.polylines(image, [bbox],isClosed, color, thickness) 
+            # plt.imshow(image)
+            # plt.show()
